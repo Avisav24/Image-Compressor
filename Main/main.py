4604ef0a3a0b74ca207da2998067c30f8f30f10e
Abhinav Vats <142833016+Avisav24@users.noreply.github.com>
2025-07-03 17:13:07 +0530

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -19,6 +19,6 @@ def compress_image(input_path, output_path, quality=85):
         print(f"An error occurred during compression: {e}")
 
 if __name__ == "__main__":
-    input_image_file = r"Enter_photo_path_here"
+    input_image_file = r"Enter_photo_path_here" #Here you need to enter the path of the photo you want to compress!
     output_compressed_file = r"Enter_your_destination enter_the_name_of_file.jpg"
     compress_image(input_image_file, output_compressed_file, quality=62)
