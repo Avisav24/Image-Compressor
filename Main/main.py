d054751d8a2f1b965cbb8acec880ef77406396dc
Abhinav Vats <142833016+Avisav24@users.noreply.github.com>
2025-07-19 13:46:06 +0530

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -17,7 +17,7 @@ def compress_image(input_path, output_path, quality=85):
         print(f"Error: Input file not found at {input_path}")
     except Exception as e:
         print(f"An error occurred during compression: {e}")
-
+#Addpath
 if __name__ == "__main__":
     input_image_file = r"Enter_photo_path_here" #Here you need to enter the path of the photo you want to compress!
     output_compressed_file = r"Enter_your_destination enter_the_name_of_file.jpg"   #Here enter the destination path of the desired location with gap then enter the file name followed by .jpg extention! 
